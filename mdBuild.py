ed8302f8db372b3c70143ad56861fad7038c1ca7
Chris White <white238@llnl.gov>
2010-11-05 14:21:14 -0700

    *Move away from using target.buildSystems that is populated at the beginning of the build process.  It changes after steps in the build process

--- mdBuild.py
+++ mdBuild.py
@@ -7,21 +7,17 @@ def build(target, options):
         if options.verbose:
             Logger().reportStart(target.name, "build")
         returnCode = None
+        targetPath = target.path
         outFd = Logger().getOutFd(target.name, "build")
         if target.buildCmd != "":
             returnCode = executeSubProcess(target.buildCmd.split(" "), target.path, outFd, options.verbose, True)
         else:
-            if "make" in target.buildSystems:
-                makefile = findShallowestFile(target.path, ["GNUmakefile", "makefile", "Makefile"])
-                wd = includeTrailingPathDelimiter(os.path.dirname(makefile))
-                returnCode = executeSubProcess(["make"], wd, outFd, options.verbose, False)
-            elif "automake" in target.buildSystems:
-                makefile = findShallowestFile(target.path, ["GNUmakefile", "makefile", "Makefile"])
-                if makefile == "":
-                    Logger().writeError(message="Automake did not create a Makefile", targetName=target.name, targetStep="build", exit=False)
-                    Logger().reportFailure(target.name, "build", 1, True)
-                wd = includeTrailingPathDelimiter(os.path.dirname(makefile))
-                returnCode = executeSubProcess(["make"], wd, outFd, options.verbose, False)
+            for item in os.listdir(targetPath):
+                itemPath = includeTrailingPathDelimiter(targetPath) + item
+                if os.path.isfile(itemPath):
+                    basename = os.path.basename(item)
+                    if str.lower(basename) in ["GNUmakefile", "makefile", "Makefile"]:
+                        returnCode = executeSubProcess(["make"], targetPath, outFd, options.verbose, False)
         if returnCode == None:
             Logger().reportSkipped(target.name, "build")
         elif returnCode != 0:
--- mdInstall.py
+++ mdInstall.py
@@ -7,21 +7,17 @@ def install(target, options):
         if options.verbose:
             Logger().reportStart(target.name, "install")
         returnCode = None
+        targetPath = target.path
         outFd = Logger().getOutFd(target.name, "install")
         if target.installCmd != "":
             returnCode = executeSubProcess(target.installCmd.split(" "), target.path, outFd, options.verbose, True)
         else:
-            if "make" in target.buildSystems:
-                makefile = findShallowestFile(target.path, ["GNUmakefile", "makefile", "Makefile"])
-                wd = includeTrailingPathDelimiter(os.path.dirname(makefile))
-                returnCode = executeSubProcess(["make", "install"], wd, outFd, options.verbose, False)
-            elif "automake" in target.buildSystems:
-                makefile = findShallowestFile(target.path, ["GNUmakefile", "makefile", "Makefile"])
-                if makefile == "":
-                    Logger().writeError(message="Automake did not create a Makefile", targetName=target.name, targetStep="install", exit=False)
-                    Logger().reportFailure(target.name, "install", 1, True)
-                wd = includeTrailingPathDelimiter(os.path.dirname(makefile))
-                returnCode = executeSubProcess(["make", "install"], wd, outFd, options.verbose, False)
+            for item in os.listdir(targetPath):
+                itemPath = includeTrailingPathDelimiter(targetPath) + item
+                if os.path.isfile(itemPath):
+                    basename = os.path.basename(item)
+                    if str.lower(basename) in ["GNUmakefile", "makefile", "Makefile"]:
+                        returnCode = executeSubProcess(["make", "install"], targetPath, outFd, options.verbose, False)
         if returnCode == None:
             Logger().reportSkipped(target.name, "install")
         elif returnCode != 0:
