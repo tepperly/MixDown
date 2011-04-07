4126c1ce432de0d3369e42c18bf82a463f799fb0
Chris White <white238@llnl.gov>
2011-04-07 16:51:45 -0700

    Add dependancy finding code

--- mdAutoTools.py
+++ mdAutoTools.py
@@ -22,6 +22,8 @@
 
 import os, re, mdMake, mdStrings, mdTarget, utilityFunctions
 
+from mdLogger import *
+
 def isAutoToolsProject(path):
     path = utilityFunctions.includeTrailingPathDelimiter(path)
     if os.path.exists(path + "configure") or os.path.exists(path + "configure.ac") or os.path.exists(path + "configure.in"):
@@ -35,6 +37,44 @@ def getInstallDir(command):
     if match != None:
         prefix = match.group(1)
     return prefix
+
+def getDependancies(path, name=""):
+    deps = []
+    if not os.path.isdir(path):
+        return None
+    if not os.path.exists(path + "/configure.ac") and not os.path.exists(path + "/configure"):
+        return None
+
+    if os.path.exists(path + "/configure.ac"):
+        Logger().writeMessage("Running autoreconf to create build files...", name)
+        returnCode = utilityFunctions.executeSubProcess("autoreconf -i", path)
+        if returnCode != 0:
+            return None
+
+    Logger().writeMessage("Analyzing 'configure --help' output", name)
+    helpFileName = path + "/configure_help.log"
+    helpFile = open(helpFileName, "w")
+    try:
+        utilityFunctions.executeSubProcess("./configure --help", path, helpFile.fileno())
+    finally:
+        helpFile.close()
+    if returnCode != 0:
+        return None
+
+    try:
+        helpFile = open(helpFileName, "r")
+        regexp = re.compile(r"--with-([a-zA-Z\-_]+)=(?:PREFIX|PATH|DIR)")
+        for line in helpFile:
+            match = regexp.search(line)
+            if match != None:
+                foundDep = match.group(1)
+                foundDep = mdTarget.normalizeName(foundDep)
+                if not foundDep in deps:
+                    deps.append(foundDep)
+    finally:
+        helpFile.close()
+
+    return deps
 
 # Commands
 def getPreconfigureCommand():
