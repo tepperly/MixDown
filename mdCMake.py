ad7e2a85fe6645e3253d3eb7f466baab3335133f
Chris White <white238@llnl.gov>
2011-04-14 22:26:50 -0700

    Add getDependancy work.. this needs some more work (find_library reg exp isn't working like it should)

--- mdCMake.py
+++ mdCMake.py
@@ -30,11 +30,59 @@ def isCMakeProject(path):
 
 def getInstallDir(command):
     prefix = ""
-    regex = re.compile(r"-DCMAKE_INSTALL_PREFIX=([A-Za-z0-9\/\.]+)")
+    regex = re.compile(r"cmake.*-DCMAKE_INSTALL_PREFIX=([A-Za-z0-9\/\.]+)")
     match = regex.search(command)
     if match != None:
         prefix = match.group(1)
     return prefix
+
+def _findAllCMakeFiles(path, listToBeFilled):
+    for name in dirList:
+        fullPath = utilityFunctions.includeTrailingPathDelimiter(path) + name
+        if os.path.isdir(fullPath):
+            _findAllCMakeFiles(path, listToBeFilled)
+        elif name == "CMakeLists.txt":
+            listToBeFilled.append(fullPath)
+        elif name.endswith(".cmake"):
+            listToBeFilled.append(fullPath)
+
+def getDependancies(path, name="", verbose=True):
+    deps = []
+    if not os.path.isdir(path):
+        return None
+    if not isCMakeProject(path):
+        return None
+
+    if verbose:
+        Logger().writeMessage("Analyzing CMake files for dependancies...", name)
+
+    fileList = list()
+    _findAllCMakeFiles(path, fileList)
+
+    packageRegExp = re.compile(r"find_package\(\s*([^\)]+\s)\)")
+    #TODO: look more into this... python regular expressions only return the last match it found
+    libraryRegExp = re.compile(r"find_library\(\s*[\w_]+(?:NAMES)?([^\)]+(?:HINTS|PATHS|PATH_SUFFIXES|DOC|NO_DEFAULT_PATH|NO_CMAKE_ENVIRONMENT_PATH|NO_CMAKE_PATH|NO_SYSTEM_ENVIRONMENT_PATH|NO_CMAKE_SYSTEM_PATH|CMAKE_FIND_ROOT_PATH_BOTH|ONLY_CMAKE_FIND_ROOT_PATH|NO_CMAKE_FIND_ROOT_PATH)*\)")
+    for name in fileList:
+        try:
+            cmakeFile = open(name, "r")
+            for line in cmakeFile:
+                match = packageRegExp.search(line)
+                if match != None:
+                    foundDep = mdTarget.normalizeName(match.group(1))
+                    if not foundDep in deps:
+                        deps.append(foundDep)
+                    continue
+                #TODO: look at todo above
+                match = libraryRegExp.search(line)
+                if match != None:
+                    for item in match.group:
+                        print item
+                        
+                
+        finally:
+            cmakeFile.close()
+
+    return deps
 
 # Commands
 def getPreconfigureCommand():
