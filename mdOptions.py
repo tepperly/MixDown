6c0514f1ec3c94c0f27d1bf32cc2407cf9b42d5b
Chris White <white238@llnl.gov>
2011-06-23 16:32:32 -0700

    fix typo

--- mdOptions.py
+++ mdOptions.py
@@ -198,7 +198,7 @@ class Options(object):
                 validateOption(currFlag, currValue)
                 self.verbose = True
             elif currFlag == "-s":
-                ValidateOption(currFlag, currValue)
+                validateOption(currFlag, currValue)
                 self.skipSteps = currValue
             elif currArg.lower() in ("/help", "/h", "-help", "--help", "-h"):
                 self.printUsageAndExit()
