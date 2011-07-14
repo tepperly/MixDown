200199ffc159776b611f9cc16890d56a75be99a2
Chris White <white238@llnl.gov>
2011-07-13 19:58:44 -0700

    rename mdStrings to mdDefines

--- mdStrings.py
+++
@@ -1,7 +0,0 @@
-mdDefinePrefix = "_prefix"
-mdDefineJobSlots = "_jobslots"
-
-#GNU make strings
-mdMakeJobSlotsPrefix = "-j"
-mdMakeJobSlotsDefineName = "_makejobslots"
-mdMakeJobSlotsDefineValue = mdMakeJobSlotsPrefix + "$(" + mdDefineJobSlots + ")"
