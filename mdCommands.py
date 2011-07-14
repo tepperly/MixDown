58f17f26bfe267890829f94dad3c0a7b0b33cabe
Chris White <white238@llnl.gov>
2011-07-13 19:51:07 -0700

    general defines updating

--- mdCommands.py
+++ mdCommands.py
@@ -21,7 +21,7 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 import os, time
-import mdAutoTools, mdCMake, mdMake, mdOptions, mdPython, mdStrings, mdTarget, utilityFunctions
+import mdAutoTools, mdCMake, mdMake, mdOptions, mdPython, mdDefines, mdTarget, utilityFunctions
 
 from mdLogger import *
 
