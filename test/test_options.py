866e12fd08ecc838d830758e0b275def18486f0a
Chris White <white238@llnl.gov>
2012-01-05 15:13:06 -0800

    Add some test functions for options.py.. more to come

--- test/test_options.py
+++ test/test_options.py
@@ -48,6 +48,120 @@ class Test_options(unittest.TestCase):
         commandline = "MixDown"
         self.assertEquals(testOptions.processCommandline(commandline.split(" ")), False, "Command line should not have processed correctly")
 
+    def test_processCommandline05(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            url = "http://www.webdav.org/neon/neon-0.29.5.tar.gz"
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+            directory = os.path.join(tempDir, "c")
+            os.mkdir(directory)
+
+            testOptions = options.Options()
+            commandline = "MixDown --import " + url + " " + tarPath + " " + directory
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), True, "Command line should have processed correctly")
+            self.assertEquals(len(testOptions.targetsToImport), 3, "Number of targets to import was wrong")
+            self.assertEquals(testOptions.targetsToImport[0].name, "neon", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[0].path, url, "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[1].name, "test", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[1].path, tarPath, "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[2].name, "c", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[2].path, directory, "Target had wrong name")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
+    def test_processCommandline06(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+
+            testOptions = options.Options()
+            commandline = "MixDown --import " + tarPath
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), True, "Command line should have processed correctly")
+            self.assertEquals(len(testOptions.targetsToImport), 1, "Number of targets to import was wrong")
+            self.assertEquals(testOptions.targetsToImport[0].name, "test", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[0].path, tarPath, "Target had wrong name")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
+    def test_processCommandline07(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+
+            testOptions = options.Options()
+            commandline = "MixDown --import " + tarPath + " " + tarPath
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), True, "Command line should have processed correctly")
+            self.assertEquals(len(testOptions.targetsToImport), 2, "Number of targets to import was wrong")
+            self.assertEquals(testOptions.targetsToImport[0].name, "test", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[0].path, tarPath, "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[1].name, "test", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[1].path, tarPath, "Target had wrong name")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
+    def test_processCommandline08(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+
+            testOptions = options.Options()
+            commandline = "MixDown " + tarPath + " --import"
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), True, "Command line should have processed correctly")
+            self.assertEquals(len(testOptions.targetsToImport), 1, "Number of targets to import was wrong")
+            self.assertEquals(testOptions.targetsToImport[0].name, "test", "Target had wrong name")
+            self.assertEquals(testOptions.targetsToImport[0].path, tarPath, "Target had wrong name")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
+    def test_processCommandline09(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+
+            testOptions = options.Options()
+            commandline = "MixDown " + tarPath + " --import --clean"
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), False, "Command line should have processed correctly")
+            self.assertEquals(testOptions.targetsToImport, [], "targetsToImport should have not been set")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
+    def test_processCommandline10(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+            projectFilePath = os.path.join(tempDir, "test.md")
+            mdTestUtilities.createBlankFile(projectFilePath)
+
+            testOptions = options.Options()
+            commandline = "MixDown " + tarPath + " --import --clean " + projectFilePath
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), False, "Command line should have processed correctly")
+            self.assertEquals(testOptions.targetsToImport, [], "targetsToImport should have not been set")
+            self.assertEquals(testOptions.projectFile, "", "projectFile should not have been set")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
+    def test_processCommandline11(self):
+        try:
+            tempDir = mdTestUtilities.makeTempDir()
+            tarDir, tarFile = mdTestUtilities.createTarFile(tempDir)
+            tarPath = os.path.join(tempDir, tarFile)
+            projectFilePath = os.path.join(tempDir, "test.md")
+            mdTestUtilities.createBlankFile(projectFilePath)
+
+            testOptions = options.Options()
+            commandline = "MixDown " + tarPath + " " + projectFilePath
+            self.assertEquals(testOptions.processCommandline(commandline.split(" ")), False, "Command line should have processed correctly")
+            self.assertEquals(testOptions.targetsToImport, [], "targetsToImport should have not been set")
+            self.assertEquals(testOptions.projectFile, "", "projectFile should not have been set")
+        finally:
+            utilityFunctions.removeDir(tempDir)
+
     def test_validate01(self):
         try:
             tempDir = mdTestUtilities.makeTempDir()
